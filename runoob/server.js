50e2837484b0f11cf2bd25dbc65d27fa13a33cb2
Jimmy.Li <Jimmy.Li@msn.com>
2017-02-21 14:01:12 +0800

    require http  created web server.

--- runoob/server.js
+++ runoob/server.js
@@ -1,9 +1,25 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
 
 http.createServer(function(request, response){
     var path = url.parse(request.url).pathname;
+    if(path.indexOf('favicon') != -1){
+        return ;
+    }
+
+    fs.readFile( 'runoob/' + path.substr(1), function(err, data){
+        if(err){
+            console.log(err);
+            response.writeHead(404, {'Content-Type': 'text/html'});
+        } else{
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(data.toString());
+        }
+
+        response.end();
+    })
 
 }).listen(6001);
 
