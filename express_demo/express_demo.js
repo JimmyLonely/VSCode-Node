a744653aa9cab6c807516946c1b693be7a554ffe
Jimmy.Li <Jimmy.Li@msn.com>
2017-02-22 14:56:23 +0800

    express upload file.

--- express_demo/express_demo.js
+++ express_demo/express_demo.js
@@ -1,12 +1,14 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser')
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var multer = require('multer');
 
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(multer({dest: '/tmp/'}).array('image'));
 
 app.get('/index.html', function(req, res){
     res.sendFile(__dirname + '/' + 'index.html');
@@ -23,7 +25,7 @@ app.get('/process_get', function(req, res){
 
 });
 
-app.post('/process_post',urlencodedParser, function(req, res){
+app.post('/process_post', function(req, res){
     var response = {
         firstName: req.body.first_name,
         lastName: req.body.last_name
@@ -31,6 +33,32 @@ app.post('/process_post',urlencodedParser, function(req, res){
 
     res.send(JSON.stringify(response));
 
+});
+
+app.post('/file_upload', function(req, res){
+
+    var upload_file = req.files[0];
+    var des_file = __dirname + '/' + upload_file.originalname;
+
+    fs.readFile(upload_file.path, function(err, data){
+        if(err){
+            return console.log(err);
+        }
+
+        fs.writeFile(des_file, data, function(err){
+            if(err){
+                return console.log(err);   
+            } else  {
+                response = {
+                    message: 'File upload successfully!',
+                    filename: upload_file.originalname
+                }
+            }
+
+            //console.log(response);
+            res.send(JSON.stringify(response));
+        })
+    })
 });
 
 var server = app.listen(6001, function(){
